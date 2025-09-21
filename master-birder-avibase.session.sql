8777e5860fb75b359edd17e84e49f30afce258f1
Ken Shih <ken.shih@gmail.com>
2025-09-21 16:34:09 -0700

    load rest of taxon data. missing parent child table data

--- master-birder-avibase.session.sql
+++ master-birder-avibase.session.sql
@@ -1,9 +1,13 @@
--- select * 
--- from AvibaseID aid
--- join TaxanomicConcepts tc on aid.avibase_id = tc.avibase_id
+select * 
+from AvibaseID aid
+join TaxanomicConcepts tc on aid.avibase_id = tc.avibase_id
+where tc.concept_id like 'SPECIES-%'
+limit 10
+/*
+-- enable this:
+-- select *
+-- from OriginalConcepts oc
+-- join TaxanomicConcepts tc on oc.concept_id = tc.concept_id
+-- where higher_classification = 'Galliformes'
 -- limit 10;
-
-select *
-from OriginalConcepts oc
-join TaxanomicConcepts tc on oc.concept_id = tc.concept_id
-limit 10;
+*/
