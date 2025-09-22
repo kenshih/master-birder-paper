559058bee35514855b1d6a1bf7534a70b6f7e05b
Ken Shih <ken.shih@gmail.com>
2025-09-21 22:40:21 -0700

    fix problem rows
    reload tables
    
    load parent-child relationships

--- master-birder-avibase.session.sql
+++ master-birder-avibase.session.sql
@@ -1,9 +1,45 @@
+select *
+FROM AvibaseID
+where concept_label in ('Myiothlypis griseiceps', 'Aegotheles albertisi')
+/*
+SELECT *
+FROM TaxanomicConcepts
+where concept_id in ('SPECIES-5792', 'SPECIES-32087')
+
+
+select *
+FROM OriginalConcepts
+where concept_id in ('SPECIES-5792', 'SPECIES-32087');
+
+
+
+SELECT *
+FROM TaxanomicConcepts
+WHERE avibase_id = ''
+
+sanity check for parent child relationships of families to orders
+SELECT 
+    f.higher_classification,
+    f.scientific_name family_name,
+    o.scientific_name order_name,
+    f.avibase_id,
+    'AL25' as version,
+    o.avibase_id as parent_id,
+    1.0 as fract_weight
+FROM TaxanomicConcepts f
+JOIN TaxanomicConcepts o ON f.higher_classification = 'Order: ' || o.scientific_name
+WHERE f.concept_id LIKE 'FAMILY-%'
+  AND o.concept_id LIKE 'ORDER-%'
+select *
+from ParentChildRelationships
+limit 10;
+
 select * 
 from AvibaseID aid
 join TaxanomicConcepts tc on aid.avibase_id = tc.avibase_id
 where tc.concept_id like 'SPECIES-%'
 limit 10
-/*
+
 -- enable this:
 -- select *
 -- from OriginalConcepts oc
